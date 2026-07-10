d8aaf3e167674a472e738f6de8a649605847c406
Erika <erikalopespro@gmail.com>
2026-07-10 15:21:18 -0300

    corrige indentação no interceptor axios Closes  #25

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -10,7 +10,7 @@ api.interceptors.request.use((config) => {
     const token = localStorage.getItem('roteirize_token')
 
     if(token){
-        config.headers.Authorization = 'Bearer ${token}'
+        config.headers.Authorization = `Bearer ${token}`
     }
 
     return config
