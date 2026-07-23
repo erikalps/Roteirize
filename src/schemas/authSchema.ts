4d87b0e01c8497a249376770c065aaf7026ee327
Erika <erikalopespro@gmail.com>
2026-07-23 11:28:55 -0300

    usa transform para normalizar cadastro de emails

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const loginSchema = z.object({
-  email: z.email({ error: 'Email inválido ou ausente' }),
+  email: z.email({ error: 'Email inválido ou ausente' }).transform(val => val.toLowerCase().trim()),
   password: z
     .string({ error: 'Senha é obrigatória' })
     .min(1, 'Senha é obrigatória')
