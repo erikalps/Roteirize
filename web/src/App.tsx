27714f0e4a2c546dcea1da15269adef4dc9c932a
Erika <erikalopespro@gmail.com>
2026-07-27 19:23:26 -0300

    feat/40 Corrige rota e caminhos em app.tsx Closes #40

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router'
 import SingUp from "./pages/SignUp"
 import Login from "./pages/Login"
 import Dashboard from './pages/Dashboard'
+import { Navigate } from 'react-router'
 
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
           <Route path='/dashboard' element={<Dashboard/>}/>
           <Route path="/signup" element={<SingUp />} />
           <Route  path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
        </Routes>
 
     
